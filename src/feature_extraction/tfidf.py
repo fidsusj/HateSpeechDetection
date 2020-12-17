eab09afedc7cf51d95136aa143168a686129b50b
Nils Krehl <nils.krehl@lherk.de>
2020-12-17 10:04:47 +0100

    feature_extraction: refactor tfidf visualization

--- src/feature_extraction/tfidf.py
+++ src/feature_extraction/tfidf.py
@@ -5,9 +5,10 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 
 class TfIdf:
-    def __init__(self):
-        # TODO: adapt and pass parameters
-        self.vectorizer = CountVectorizer(stop_words="english", min_df=2)
+    def __init__(self, passed_min_df, passed_stop_words=None):
+        self.vectorizer = CountVectorizer(
+            stop_words=passed_stop_words, min_df=passed_min_df
+        )
 
     def calculate_tfidf(self, list_of_docs):
         tf = self._calculate_tf(list_of_docs)
@@ -26,6 +27,23 @@ class TfIdf:
         )
         return df_tf_idf
 
+    def visualize_as_heatmap(self, df_tfifd, top_n_items):
+        df_tfidf_sorted = df_tfifd.sort_values(by=[0, 1], axis=1, ascending=False)
+        df_tfidf_sorted_top_n = df_tfidf_sorted[df_tfidf_sorted.columns[:top_n_items]]
+        df_tfidf_sorted_top_n.index = ["hate", "neutral"]
+
+        fig, ax = plt.subplots(figsize=(17, 6))
+        sns.heatmap(
+            df_tfidf_sorted_top_n,
+            annot=True,
+            cbar=True,
+            ax=ax,
+            xticklabels=df_tfidf_sorted.columns[:top_n_items],
+            cmap="YlGnBu",
+        )
+        plt.show()
+        plt.savefig("../../doc/milestone/figures/Heatmap_tfidf.png")
+
 
 if __name__ == "__main__":
     df_dataset = pd.read_csv("../data/preprocessed/dataset.csv", index_col=0)
@@ -37,23 +55,7 @@ if __name__ == "__main__":
 
     list_of_docs = [list_hate_speech_as_one_doc, list_neutral_speech_as_one_doc]
 
-    df_tfifd = TfIdf().calculate_tfidf(list_of_docs)
+    tfidf = TfIdf(2, "english")
 
-    df_tfidf_sorted = df_tfifd.sort_values(by=[0, 1], axis=1, ascending=False)
-
-    n = 20
-    df_tfidf_sorted_top_n = df_tfidf_sorted[df_tfidf_sorted.columns[:n]]
-
-    df_tfidf_sorted_top_n.index = ["hate", "neutral"]
-
-    fig, ax = plt.subplots(figsize=(17, 6))
-    sns.heatmap(
-        df_tfidf_sorted_top_n,
-        annot=True,
-        cbar=True,
-        ax=ax,
-        xticklabels=df_tfidf_sorted.columns[:n],
-        cmap="YlGnBu",
-    )
-    plt.show()
-    print("Ende")
+    df_tfifd = tfidf.calculate_tfidf(list_of_docs)
+    tfidf.visualize_as_heatmap(df_tfifd, 20)
