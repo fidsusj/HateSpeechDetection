e3f7d73bb1f33a64683da40505b2a95ec4b236f1
Nils Krehl <nils.krehl@lherk.de>
2021-01-13 19:47:05 +0100

    visualize feature special characters (? ! .) as barplot

--- src/feature_extraction/semantic/special_characters.py
+++ src/feature_extraction/semantic/special_characters.py
@@ -1,5 +1,7 @@
 """ Module for feature extraction based on special characters (e.g. ? ! .) """
 
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 
@@ -24,6 +26,7 @@ class SpecialCharacters:
             df = self._count_number_of_special_characters(
                 key, self.list_of_special_characters[key], df
             )
+        self.visualize_special_characters(df)
         return df
 
     def _count_number_of_special_characters(self, character_name, character, df):
@@ -37,10 +40,57 @@ class SpecialCharacters:
     def _count_character(sentence, character):
         return sentence.count(character)
 
+    def visualize_special_characters(self, df):
+        """Visualizes the number of special characters as bar plot
+        Parameters:
+            df: dataframe with the extracted features for special characters
+        Return:
+            stores barplots in analysis folder
+        """
+        df_hate_speech = df[df["class"] == 0]
+        df_neutral_speech = df[df["class"] == 1]
+        for character in self.list_of_special_characters:
+            hate_bincount = self._calculate_bincount_of_special_character(
+                df_hate_speech, character
+            )
+            neutral_bincount = self._calculate_bincount_of_special_character(
+                df_neutral_speech, character
+            )
+
+            hate_bincount_summarized = self._summarize_bincount_data(hate_bincount)
+            neutral_bincount_summarized = self._summarize_bincount_data(
+                neutral_bincount
+            )
+
+            x = np.arange(11)
+            plt.bar(x + 0.0, hate_bincount_summarized, color="r", width=0.2)
+            plt.bar(x + 0.2, neutral_bincount_summarized, color="b", width=0.2)
+            x_ticks = []
+            [x_ticks.append(str(x)) for x in range(10)]
+            x_ticks.append(">10")
+            plt.xticks(x, x_ticks)
+            plt.title(
+                "Number of data instances with number of "
+                + self.list_of_special_characters[character]
+            )
+            plt.xlabel("Number of " + character)
+            plt.ylabel("Number of data instances")
+            plt.legend(["hate speech", "neutral speech"])
+            plt.show()
+            plt.savefig(
+                "../../analysis/features/semantic/barchart_special_character_"
+                + character
+            )
+
+    def _calculate_bincount_of_special_character(self, df, character):
+        return np.bincount(np.array(df["number_of_" + character]))
+
+    def _summarize_bincount_data(self, array):
+        return np.append(array[:10], array[10:].sum())
+
 
 if __name__ == "__main__":
     df_dataset = pd.read_csv("../../data/preprocessed/dataset.csv", index_col=0)
 
     special_characters = SpecialCharacters()
     df_with_extracted_features = special_characters.extract_features(df_dataset)
-    print(df_with_extracted_features)
