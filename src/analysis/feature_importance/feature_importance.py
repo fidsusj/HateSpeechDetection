f5630ab8c71db07f72991ac36b5b3463bc577bd2
Felix Hausberger <felix.hausberger@sap.com>
2021-02-21 16:27:58 +0100

    main: some changes on presentation

--- src/analysis/feature_importance/feature_importance.py
+++ src/analysis/feature_importance/feature_importance.py
@@ -7,6 +7,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 # from sklearn.svm import SVC
+from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
 
@@ -21,7 +22,7 @@ class FeatureImportance:
             random_state=42,
         )
 
-        # self.classifier_SVC = SVC(kernel="linear")
+        self.classifier_SVC = SVC(kernel="linear")
         self.classifier_logistic_regression = LogisticRegression(
             max_iter=1500, C=1.0, solver="lbfgs"
         )
@@ -31,7 +32,7 @@ class FeatureImportance:
         self.classifier_decision_tree = DecisionTreeClassifier(criterion="entropy")
         self.feature_names = feature_names
 
-        # self.classifier_SVC.fit(X_train, y_train)
+        self.classifier_SVC.fit(X_train, y_train)
         self.classifier_logistic_regression.fit(X_train, y_train)
         self.classifier_random_forest.fit(X_train, y_train)
         self.classifier_decision_tree.fit(X_train, y_train)
@@ -41,7 +42,7 @@ class FeatureImportance:
 
         data = np.array(
             [
-                # self.classifier_SVC.coef_[0],
+                self.classifier_SVC.coef_[0],
                 self.classifier_logistic_regression.coef_[0],
                 self.classifier_random_forest.feature_importances_,
                 self.classifier_decision_tree.feature_importances_,
@@ -50,7 +51,7 @@ class FeatureImportance:
 
         df = pd.DataFrame(
             data=data,
-            index=["Logistic Regression", "Random Forest", "Decision Tree"],
+            index=["SVM", "Logistic Regression", "Random Forest", "Decision Tree"],
             columns=self.feature_names,
         )
         with pd.option_context("display.max_rows", None, "display.max_columns", None):
